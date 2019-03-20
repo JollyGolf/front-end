0dec5ad954fd5195db5c6c7d8eb4cd7db893c2ed
jollygolf <zhigamovky>
2019-03-20 11:17:56 +0200

    Dealt with 'Manipulation with DOM

---
+++ javascript/manipulation-with-DOM.js
@@ -0,0 +1,19 @@
+const root = document.getElementById("root");
+console.log(root);
+let wrapperDiv = document.createElement("div");
+root.appendChild(wrapperDiv); 
+let textWrapperDiv = document.createTextNode("XMPL");
+wrapperDiv.appendChild(textWrapperDiv);
+wrapperDiv.innerHTML = '';
+wrapperDiv.className = 'wrapper-flexbox';
+
+for(let i = 0; i < 200; i++) {
+  let elementDiv = document.createElement("div");
+  let textItem = document.createTextNode('item-'+i);
+  elementDiv.setAttribute('data-id', i);
+  elementDiv.appendChild(textItem);
+  wrapperDiv.appendChild(elementDiv);
+  elementDiv.setAttribute('id', i);
+  elementDiv.className = 'item item-'+i;
+}
+
