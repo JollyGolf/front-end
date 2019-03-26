b4e9654c3b4031c3ac621d088f99445c9988d34a
jollygolf <zhigamovky>
2019-03-26 12:40:06 +0200

    Added package-lock.json and package.json to previus commit

--- graphql/server/app.js
+++ graphql/server/app.js
@@ -1,6 +1,9 @@
-const express = require('express');
+const express = require('express'); 
+const graphqlHTTTP = require('express-graphql');
+const schema = require('./schema/schema');
+
 const app = express();
-
-//require('express')().listen(4000, () => console.log('Now listening for requests on port 4000'));
-
+app.use('/graphql', graphqlHTTTP({
+  schema          //equal schema: schema (both same name)
+}));
 app.listen(4000, () => console.log('Now listening for requests on port 4000'));
