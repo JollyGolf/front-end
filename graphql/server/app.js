fbb2879d863c589bcabbeb13414d9354efc95b65
jollygolf <zhigamovky>
2019-03-27 18:40:24 +0200

    Installed 'cors', rendered data in a component

--- graphql/server/app.js
+++ graphql/server/app.js
@@ -2,11 +2,18 @@ const express = require('express');
 const graphqlHTTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
+const app = express();
+
+//allow cross-origin requests
+app.use(cors());
+
+//connect to mongoDB
 mongoose.connect('mongodb+srv://zhigamovsky:qwertykybcem@cluster0-mpgcc.mongodb.net/test?retryWrites=true');
 mongoose.connection.once('open', () => console.log('\nConnected to MongoDB\n'));
 
-const app = express();
+
 app.use('/graphql', graphqlHTTTP({
   schema: schema,
   graphiql: true
